e3f86cb003de46329fdaff6c829fb1c1147326da
sanka01 <sanka1375@gmail.com>
2023-11-05 17:29:14 -0300

    correcao de bug

--- scr/view/QuadroComum.js
+++ scr/view/QuadroComum.js
@@ -11,6 +11,7 @@ export class QuadroComum extends Component {
                 <Quadro 
                     url="selectDadosComum.php" 
                     data= {{setor: this.props.route.params.id}}
+                    navigation = {this.props.navigation}
                     renderizaMusicos/>
             </View>
         )
